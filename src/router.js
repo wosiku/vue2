74b5f141a7787fa486f5f411d732e065a4379ec4
xiezhangrui <276357939@qq.com>
2026-02-25 09:07:31 +0800

    redirect: '/home'

--- src/router.js
+++ src/router.js
@@ -6,7 +6,9 @@ import Home from '@/views/home'
 Vue.use(VueRouter)
 
 const routes = [
-  { path: '/', component: Home },
+  { path: '/home', component: Home },
+  { path: '/', redirect: '/home' },
+  { path: '*', redirect: '/home' }
 ]
 
 const router = new VueRouter({
